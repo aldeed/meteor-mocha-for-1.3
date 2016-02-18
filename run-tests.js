b5ae6c7cae37d4339e2710b08c4d18b2742d7f3b
Avital Oliver <avital@thewe.net>
2016-02-17 23:00:14 -0800

    Process style tags even with multiple stylesheets

--- run-tests.js
+++ run-tests.js
@@ -6,15 +6,18 @@ runTests = function() {
   MochaRunner.setReporter(practical.mocha.HtmlReporter);
 
   inRange = false;
-  var rules = _.filter(document.styleSheets[0].cssRules, (r) => {
-    if (r.selectorText && r.selectorText.match(/scoped/)) {
-      inRange = !inRange;
-    };
-    return inRange;
+  _.each(document.styleSheets, (sheet) => {
+    var rules = _.filter(sheet.cssRules, (r) => {
+      if (r.selectorText && r.selectorText.match(/scoped/)) {
+        inRange = !inRange;
+      };
+      return inRange;
+    });
+
+    var styles = _.pluck(rules, 'cssText').join('\n');
+
+    $('head').append('<style>' + styles + '</style>');
   });
 
-  var styles = _.pluck(rules, 'cssText').join('\n');
-
   $('head link[rel=stylesheet]').remove();
-  $('head').append('<style>' + styles + '</style>');
 };
